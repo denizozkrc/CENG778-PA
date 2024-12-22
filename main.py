b73e60a62323bcca77c430748ae71bdcccee966c
denizozkiric <ozkiric.deniz@metu.edu.tr>
2024-12-22 23:44:19 +0300

    final version

--- main.py
+++ main.py
@@ -1,25 +1,62 @@
 from searchEngineSimulator import SearchEngineSimulator
+import matplotlib.pyplot as plt
+
+k_values = [8, 32]
 
 dataset = (open(file="./datasets/wordlist.txt", mode="r")).read()
-
 index_dict = {}
 for line in dataset.split("\n"):
     if len(line) == 0:
         continue
     parts = line.split(" ")
     key = parts[0]
-    value = parts[1]
-    index_dict[key] = value  # We can discard the second number
+    value = [int(parts[1]), 0]  # 0 will be replaced while term partitioning
+    index_dict[key] = value  # We can discard the second number. {"eddie" : [7, 0]} PLL=7, node_num =0 (to be updated)
 
 querylist = (open(file="./datasets/10000.topics", mode="r")).read()
-
 query_list = []
 for line in querylist.split("\n"):
     if len(line) == 0:
         continue
     parts = line.split(":", 1)
     query = parts[1].split(" ")
-    query_list.append(query)
+    query_list.append(query)  # ['after', 'school', 'program', 'evaluation']
 
-simulator = SearchEngineSimulator(index_dict)
-simulator.run(query_list)
+simulator = SearchEngineSimulator(index_dict, k_values)
+
+term_based_costs, node_costs_term = simulator.run(query_list, type="term")
+doc_based_costs, total_cost_per_node = simulator.run(query_list, type="doc")
+
+average_QP_term = [0] * len(k_values)
+average_QP_doc = [0] * len(k_values)
+average_load_per_node = [0] * len(k_values)
+for k in range(len(k_values)):
+    average_QP_term[k] = sum(term_based_costs[k])/len(term_based_costs[k])
+    average_QP_doc[k] = sum(doc_based_costs[k])/len(doc_based_costs[k])
+    average_load_per_node[k] = sum(node_costs_term[k])/len(node_costs_term[k])
+
+print("Average QP cost with term-based partitioning: ", average_QP_term)
+print("Average QP cost with document-based partitioning: ", average_QP_doc)
+print("QP cost per node with document-based partitioning: ", total_cost_per_node)
+print("average QP cost per node with term-based partitioning: ", average_load_per_node)
+
+"""
+term_based_costs = [for each k][for each query]
+node_costs_term = [for each k][for each node]
+
+doc_based_costs = [for each k][for each query]
+total_cost_per_node = [for each k]
+"""
+
+for k_index, costs in enumerate(node_costs_term):
+    node_numbers = list(range(len(costs)))
+
+    # Create a new figure for each k
+    plt.figure()
+    plt.plot(node_numbers, costs, marker='o', label=f'k={len(node_numbers)-1}')
+    plt.title(f'Plot for K={len(node_numbers)-1}')
+    plt.xlabel('Node Numbers')
+    plt.ylabel('Values')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
--- searchEngineSimulator.py
+++ searchEngineSimulator.py
@@ -1,8 +1,68 @@
 class SearchEngineSimulator:
 
-    def __init__(self, indexFile):
+    def __init__(self, indexFile, k_values):
         self.indexFile = indexFile
-        self.k_values = [8, 32]
+        self.k_values = k_values
 
-    def run(self, query_list):
-        pass
+    def run(self, query_list, type):
+        if type == "term":  # Term Based Partitioning:
+            term_based_costs = [0]*len(self.k_values)
+            node_costs_term = [0]*len(self.k_values)
+            for k, K in enumerate(self.k_values):
+                total_costs = [0]*len(query_list)  # total cost for each query
+                total_cost_each_node = [0]*(K+1)  # total cost for each node
+                for i, val in enumerate(self.indexFile.values()):
+                    val[1] = (i % K)+1  # node number. 0 is the broker, so not used here
+                for q, query in enumerate(query_list):
+                    query_len = len(query)
+                    nodes = [[0, 0, 0] for _ in range(K + 1)]  # +1 for the broker. first is number of terms, second is total posting length, third is for min posting length
+                    costs = [0]*(K+1)  # index 0 is for the broker
+                    for term in query:
+                        if term not in self.indexFile:
+                            query_len -= 1
+                            continue
+                        node_num = self.indexFile[term][1]  # which node is it on
+                        nodes[node_num][0] += 1  # number of terms
+                        nodes[node_num][1] += self.indexFile[term][0]  # total posting length
+                        if self.indexFile[term][0] < nodes[node_num][2] or nodes[node_num][2] == 0:
+                            nodes[node_num][2] = self.indexFile[term][0]
+
+                    for i, node in enumerate(nodes):
+                        if i == 0:
+                            continue
+
+                        if node[0] == 0:  # no term here
+                            continue
+                        elif node[0] == query_len:  # all terms here
+                            costs[i] = node[1]  # all done in node, nothing in broker
+                        elif node[0] == 1:  # multiterm query, one term here
+                            costs[0] += node[2]  # nothing done in node, add cost on broker
+                        else:  # mulitple terms in this node, but not all
+                            costs[i] = node[1]  # total posting list cost in node
+                            costs[0] += node[2]  # min posting list cost in the broker
+                    total_costs[q] = costs[0] + max((cost for cost in costs[1:] if cost != 0), default=0)
+                    total_cost_each_node = [total + cost for total, cost in zip(total_cost_each_node, costs)]
+                term_based_costs[k] = total_costs
+                node_costs_term[k] = total_cost_each_node
+            return term_based_costs, node_costs_term
+
+        elif type == "doc":  # Document Based Partitioning:
+            doc_based_costs = [0]*len(self.k_values)
+            total_cost_per_node = [0]*len(self.k_values)
+            for k, K in enumerate(self.k_values):
+                total_costs = [0]*len(query_list)
+                for q, query in enumerate(query_list):
+                    query_len = len(query)
+                    cost = 0
+                    for term in query:
+                        if term not in self.indexFile:
+                            query_len -= 1
+                            continue
+                        cost += (self.indexFile[term][0]/K)+1  # +1 for ceiling. same cost for each node
+                    total_cost_per_node[k] += cost  # same for each node so calculate only for one node
+                    total_costs[q] = cost  # total cost of a query = cost of a query at a single node
+                doc_based_costs[k] = total_costs
+            return doc_based_costs, total_cost_per_node
+
+        else:
+            raise ValueError("Invalid partition type. Use 'term' or 'doc'.")
